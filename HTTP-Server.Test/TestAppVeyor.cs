d8b1072bdb6961b6d0a6f10bd193cb7e2a6997e0
Romashchenko Pavel <acubens96@meta.ua>
2015-09-20 17:51:33 +0300

    Add tests to solution

---
+++ HTTP-Server.Test/TestAppVeyor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HTTP_Server.Test
+{
+    [TestClass]
+    public class TestAppVeyor
+    {
+        [TestMethod, TestCategory("A")]
+        public void TestA()
+        {
+        }
+
+        [TestMethod, TestCategory("B")]
+        public void TestB()
+        {
+        }
+    }
+}
