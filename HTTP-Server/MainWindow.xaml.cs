6b7355004c1f3ca4be68d6353702cb12dda345a2
Romashchenko Pavel <acubens96@meta.ua>
2015-09-20 16:27:50 +0300

    Add StyleCop to solution

--- HTTP-Server/MainWindow.xaml.cs
+++ HTTP-Server/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HTTP_Server
+namespace HttpServer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
