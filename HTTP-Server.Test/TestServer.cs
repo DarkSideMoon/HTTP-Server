177e2942ab9495b67f6acd8fdb87f5cc636038cb
DarkSideMoon <acubens96@meta.ua>
2015-09-21 22:30:16 +0300

    Create test server post

---
+++ HTTP-Server.Test/TestServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HTTP_Server.Test
+{
+    [TestClass]
+    public class TestServer
+    {
+        [TestMethod, TestCategory("TestServerPOST")]
+        public void TestServerPOST()
+        {
+        }
+    }
+}
